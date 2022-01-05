1ba2cdecd61aba10c1474ff83e55c190e7321793
Ayşegül Uzun <76492076+ayseguluzun@users.noreply.github.com>
2022-01-05 20:33:39 +0300

    Add files via upload

---
+++ ana/CONST.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.e_kpss;
+
+/**
+ *
+ * @author Burcu
+ */
+public class CONST {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "";
+    static final String RAW_DB_URL = "jdbc:mysql://localhost.com/e_kpss?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";      
+}
